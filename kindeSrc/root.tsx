5985962c2f4b8475bce3bad11b0a2094de535fb3
Abdelrahman Zaki <me@abdelrahmanzaki.com>
2025-08-12 21:47:11 +0300

    feat: add meta tags and update page title in Root component

--- kindeSrc/root.tsx
+++ kindeSrc/root.tsx
@@ -22,12 +22,15 @@ export const Root = ({
     <html dir={request.locale.isRtl ? "rtl" : "ltr"} lang={request.locale.lang}>
       <head>
         <meta charSet="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta content="width=device-width, initial-scale=1.0" name="viewport" />
         <meta content="noindex" name="robots" />
         <meta content={getKindeCSRF()} name="csrf-token" />
         <meta content="light" name="color-scheme" />
         <meta content="nopagereadaloud" name="google" />
-        <title>{context.widget.content.page_title}</title>
+        <meta content="Login to your Incognifi account" name="description" />
+
+        <title>Login - Incognifi</title>
 
         <link href={getSVGFaviconUrl()} rel="icon" type="image/svg+xml" />
         {getKindeRequiredCSS()}
