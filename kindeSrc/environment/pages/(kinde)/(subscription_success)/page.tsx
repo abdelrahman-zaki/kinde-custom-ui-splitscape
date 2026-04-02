f6b908cd1f770ac1bb56133a184a416ff5ab5b55
Abdelrahman Zaki <me@abdelrahmanzaki.com>
2026-04-02 15:42:12 +0200

    feat(subscription): add subscription success page

---
+++ kindeSrc/environment/pages/(kinde)/(subscription_success)/page.tsx
@@ -0,0 +1,28 @@
+"use server";
+
+import { Widget } from "../../../../components/widget";
+import { DefaultLayout } from "../../../../layouts/default";
+import { Root } from "../../../../root";
+import { type KindePageEvent } from "@kinde/infrastructure";
+import React from "react";
+import { renderToString } from "react-dom/server.browser";
+
+const SSPage: React.FC<KindePageEvent> = ({ context, request }) => {
+  return (
+    <Root context={context} request={request}>
+      <div>SSPage</div>
+      <DefaultLayout>
+        <Widget
+          heading={context.widget.content.heading}
+          description={context.widget.content.description}
+        />
+      </DefaultLayout>
+    </Root>
+  );
+};
+
+// Page Component
+export default async function Page(event: KindePageEvent): Promise<string> {
+  const page = await SSPage(event);
+  return renderToString(page);
+}
